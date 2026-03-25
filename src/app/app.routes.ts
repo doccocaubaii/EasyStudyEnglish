a975989efe246a10b519a66c2f12f9c277995cce
doccocaubaii <102582078+doccocaubaii@users.noreply.github.com>
2026-03-25 19:44:21 +0700

    core:add sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {ListWordComponent} from './features/english/pages/list-word/list-word.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: ListWordComponent }
+    ]
+  },
+];
---
+++ src/app/features/english/pages/list-word/list-word.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-word',
+  imports: [],
+  templateUrl: './list-word.component.html',
+  styleUrl: './list-word.component.scss'
+})
+export class ListWordComponent {
+
+}
